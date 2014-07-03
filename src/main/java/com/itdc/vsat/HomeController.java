da5f3ce7b1f0395fd772c9546b373d9bc7a467ea
cmcacatian <clariz_thea_4@yahoo.com>
2014-07-03 13:37:49 +0800

    Pangit si Marc
    Totoo naman

--- src/main/java/com/itdc/vsat/HomeController.java
+++ src/main/java/com/itdc/vsat/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		//asas
+		//Pangit si Marc
 		return "sample";
 	}
 	
